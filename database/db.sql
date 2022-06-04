5895920349c7e29115cb4474a76c3ea9e000696a
Robson Tenorio <robson.tenorio@gmail.com>
2022-06-03 23:45:53 -0300

    FIX: Correção nos campos email e accept

--- database/db.sql
+++ database/db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 03-Jun-2022 às 23:33
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 7.4.27
+-- Host: localhost:3306
+-- Tempo de geração: 03/06/2022 às 23:28
+-- Versão do servidor: 8.0.29-0ubuntu0.20.04.3
+-- Versão do PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,31 +21,31 @@ USE `leads`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `bot`
+-- Estrutura para tabela `bot`
 --
 
 CREATE TABLE IF NOT EXISTS `bot` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `question` varchar(255) NOT NULL,
   `answer` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `clientes`
+-- Estrutura para tabela `clientes`
 --
 
 CREATE TABLE IF NOT EXISTS `clientes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `number` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `accept` varchar(5) NOT NULL,
-  `created_At` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
-  `update_At` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `accept` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `created_At` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `update_At` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY (`id`),
   UNIQUE KEY `number` (`number`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
